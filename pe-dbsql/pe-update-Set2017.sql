03aa4e058a6a95979bdc8c991dcb1d14b5ca02f5
Rafael de Morais <rdemorais.freitas@gmail.com>
2017-08-31 15:30:34 -0300

    atualização da etl para refletir nova funcionalidade de ativos

--- pe-dbsql/pe-update-Set2017.sql
+++ pe-dbsql/pe-update-Set2017.sql
@@ -1,1 +1,4 @@
 ALTER TABLE dbsitedemas.tb_meta_iniciativa ADD COLUMN ind_ativo character varying(1);
+ALTER TABLE dbsitedemas.tb_oe ADD COLUMN ind_ativo character varying(1);
+ALTER TABLE dbsitedemas.tb_atividade ADD COLUMN ind_ativo character varying(1);
+ALTER TABLE dbsitedemas.tb_prod_inter ADD COLUMN ind_ativo character varying(1);
